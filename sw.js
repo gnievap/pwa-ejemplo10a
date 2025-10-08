2e8825e366fe064c3d6d0f73fd7c46ec018b4482
Gaby Nieva <gaby.nieva@outlook.com>
2025-10-08 12:46:58 -0600

    Ajuste de rutas

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request).catch(
-                () =>caches.match("offline.html"));
+                () =>caches.match(`${BASE_PATH}offline.html`));
         })
     );
 });
